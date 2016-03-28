49f3d146a2d71e2569fb8934e905c994a8beab19
norgeous <daniel@harte.me>
2016-03-28 21:54:17 +0100

    code more understandable

--- useragent.js
+++ useragent.js
@@ -19,7 +19,7 @@ mac architecures:
 	Mac OS X 10.6.8 (or greater) is a 64-bit operating system
 
 windows architectures:
-	WOW64 is Windows 32-bit (process) on Windows 64-bit (OS)
+	WOW64 is Windows 32-bit (browser process) on Windows 64-bit (OS)
 
 
 */
@@ -27,116 +27,79 @@ windows architectures:
 
 $(function() {
 
-	var bits32 = ['i386', 'i486', 'i586', 'i686', 'armv7', 'armv6'];
-	var bits64 = ['x86_64', 'amd64', 'WOW64', 'Win64', 'x64', 'armv8'];
-	var ua;// = navigator.userAgent;
+	var linux32 = ['i386','i486','i586','i686','armv7','armv6'];
+	var linux64 = ['x86_64','amd64','armv8'];
+	var windo64 = ['WOW64','Win64','x64'];
 
-	function c(needle){
-		switch(typeof needle){
-			case 'string':
-				return ua.indexOf(needle) > -1;
-				break;
-			case 'object':
-				var f=false;
-				for(var i=0; i<needle.length; i++){
-					if(ua.indexOf(needle[i]) > -1) f=true;
-				}
-				return f;
-				break;
-		}
+	function c(needles,haystack){
+		var f=false;
+		for(var i=0; i<needles.length; i++)	if(haystack.indexOf(needles[i]) > -1) f=true;
+		return f;
 	};
 
-	function ua2dl(testme){
-		ua = testme;
+	function ua2dl(ua){
 		var out = '';
+		if(c(['DragonFly'],ua)	&& !c(linux32,ua))
+		out+='<div>Syncthing for Dragonfly BSD (64 bit)</div>';
 
-		if(c('DragonFly') && !c(bits32)) {
-			out+='<div>Syncthing for Dragonfly BSD (64 bit)</div>';
-		}
+		if(c(['FreeBSD'],ua)	&& c(linux32,ua))
+		out+='<div>Syncthing for FreeBSD (32 bit)</div>';
 
-		if(c('FreeBSD') && c(bits32)) {
-			out+='<div>Syncthing for FreeBSD (32 bit)</div>';
-		}
+		if(c(['FreeBSD'],ua)	&& c(linux64,ua))
+		out+='<div>Syncthing for FreeBSD (64 bit)</div>';
 
-		if(c('FreeBSD') && c(bits64)) {
-			out+='<div>Syncthing for FreeBSD (64 bit)</div>';
-		}
+		if(c(['Linux'],ua)		&& c(linux32,ua)		&& !c(['Linux arm','Android'],ua))
+		out+='<div>Syncthing for Linux (32 bit)</div>';
 
-		if(c('Linux') && !c('Linux arm') && !c('Android') && c(bits32)) {
-			out+='<div>Syncthing for Linux (32 bit)</div>';
-		}
+		if(c(['Linux'],ua)		&& c(linux64,ua)		&& !c(['Linux arm','Android'],ua))
+		out+='<div>Syncthing for Linux (64 bit)</div>';
 
-		if(c('Linux') && !c('Linux arm') && !c('Android') && c(bits64)) {
-			out+='<div>Syncthing for Linux (64 bit)</div>';
-		}
+		if(c(['Linux arm'],ua)	&& c(linux32,ua)		&& !c(['Android'],ua))
+		out+='<div>Syncthing for Linux (ARM 32-bit)</div>';
 
-		if(c('Linux arm') && c(bits32)) {
-			out+='<div>Syncthing for Linux (ARM 32-bit)</div>';
-		}
+		if(c(['Linux arm'],ua)	&& c(linux64,ua)		&& !c(['Android'],ua))
+		out+='<div>Syncthing for Linux (ARM 64-bit)</div>';
 
-		if(c('Linux arm') && c(bits64)) {
-			out+='<div>Syncthing for Linux (ARM 64-bit)</div>';
-		}
-
-		if(c('Linux') && c('ppc64')) {
-			out+='<div>Syncthing for Linux (ppc64)</div>';
-		}
+		if(c(['Linux'],ua)		&& c(['ppc64'],ua))
+		out+='<div>Syncthing for Linux (ppc64)</div>';
 
 		//Syncthing for Linux (ppc64le)
+		//help needed
 
-		if(c('Mac OS X') && !c(['iPod','iPhone','iPad'])) {
-			out+='<div>Syncthing for Mac OS X (64 bit)</div>';
-		}
+		if(c(['Mac OS X'],ua)	&& !c(['PPC','iPod','iPhone','iPad','10_6_7','10_6_6','10_6_5','10_6_4','10_6_3','10_6_2','10_6_1','10_6_0','10_5'],ua))
+		out+='<div>Syncthing for Mac OS X (64 bit)</div>';
 
-		if(c('NetBSD') && c(bits32)) {
-			out+='<div>Syncthing for NetBSD (32 bit)</div>';
-		}
+		if(c(['NetBSD'],ua)		&& c(linux32,ua))
+		out+='<div>Syncthing for NetBSD (32 bit)</div>';
 
-		if(c('NetBSD') && c(bits64)) {
-			out+='<div>Syncthing for NetBSD (64 bit)</div>';
-		}
+		if(c(['NetBSD'],ua)		&& c(linux64,ua))
+		out+='<div>Syncthing for NetBSD (64 bit)</div>';
 
-		if(c('OpenBSD') && c(bits32)) {
-			out+='<div>Syncthing for OpenBSD (32 bit)</div>';
-		}
+		if(c(['OpenBSD'],ua)	&& c(linux32,ua))
+		out+='<div>Syncthing for OpenBSD (32 bit)</div>';
 
-		if(c('OpenBSD') && c(bits64)) {
-			out+='<div>Syncthing for OpenBSD (64 bit)</div>';
-		}
+		if(c(['OpenBSD'],ua)	&& c(linux64,ua))
+		out+='<div>Syncthing for OpenBSD (64 bit)</div>';
 
-		if(c('SunOS')) {
-			out+='<div>Syncthing for Solaris/Illumos/SmartOS (64 bit)</div>';
-		}
+		if(c(['SunOS'],ua))
+		out+='<div>Syncthing for Solaris/Illumos/SmartOS (64 bit)</div>';
 
-		if(c('Win') && !c(bits64)) {
-			out+='<div>Syncthing for Windows (32 bit)</div>';
-		}
+		if(c(['Win'],ua)		&& !c(windo64,ua))
+		out+='<div>Syncthing for Windows (32 bit)</div>';
 
-		if(c('Win') && c(bits64)) {
-			out+='<div>Syncthing for Windows (64 bit)</div>';
-		}
+		if(c(['Win'],ua)		&& c(windo64,ua))
+		out+='<div>Syncthing for Windows (64 bit)</div>';
 
-		if(c('Android')) {
-			out+='<div>Android App google play or fdroid</div>';
-		}
+		if(c(['Android'],ua))
+		out+='<div>Android App google play or fdroid</div>';
 
 		return out;
 	};
 
-
 	$('#debug-you').append('<tr><td>'+navigator.userAgent+'</td><td>'+ua2dl(navigator.userAgent)+'</td></tr>');
 
-
-	// test more agents
-	$('#debug-more').hide(0);
 	for(var i=0; i<test_useragents.length; i++){
 		$('#debug-more').append('<tr><td>'+test_useragents[i]+'</td><td>'+ua2dl(test_useragents[i])+'</td></tr>');
 	}
-
-	$('#expand-more').click(function(e){
-		e.preventDefault();
-		$('#debug-more').show(1000);
-	});
-
 
 });
