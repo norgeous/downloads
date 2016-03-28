8277d66bde2e220b246712df7a54736baee07b65
norgeous <daniel@harte.me>
2016-03-28 20:50:12 +0100

    results as table

--- useragent.js
+++ useragent.js
@@ -29,7 +29,7 @@ $(function() {
 
 	var bits32 = ['i386', 'i486', 'i586', 'i686', 'armv7', 'armv6'];
 	var bits64 = ['x86_64', 'amd64', 'WOW64', 'Win64', 'x64', 'armv8'];
-	var ua = navigator.userAgent;
+	var ua;// = navigator.userAgent;
 
 	function c(needle){
 		switch(typeof needle){
@@ -46,90 +46,91 @@ $(function() {
 		}
 	};
 
-	function ua2dl(outputdiv){
+	function ua2dl(testme){
+		ua = testme;
+		var out = '';
+
 		if(c('DragonFly') && !c(bits32)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Dragonfly BSD (64 bit)</div>');
+			out+='<div>Syncthing for Dragonfly BSD (64 bit)</div>';
 		}
 
 		if(c('FreeBSD') && c(bits32)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for FreeBSD (32 bit)</div>');
+			out+='<div>Syncthing for FreeBSD (32 bit)</div>';
 		}
 
 		if(c('FreeBSD') && c(bits64)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for FreeBSD (64 bit)</div>');
+			out+='<div>Syncthing for FreeBSD (64 bit)</div>';
 		}
 
 		if(c('Linux') && !c('Linux arm') && !c('Android') && c(bits32)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Linux (32 bit)</div>');
+			out+='<div>Syncthing for Linux (32 bit)</div>';
 		}
 
 		if(c('Linux') && !c('Linux arm') && !c('Android') && c(bits64)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Linux (64 bit)</div>');
+			out+='<div>Syncthing for Linux (64 bit)</div>';
 		}
 
 		if(c('Linux arm') && c(bits32)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Linux (ARM 32-bit)</div>');
+			out+='<div>Syncthing for Linux (ARM 32-bit)</div>';
 		}
 
 		if(c('Linux arm') && c(bits64)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Linux (ARM 64-bit)</div>');
+			out+='<div>Syncthing for Linux (ARM 64-bit)</div>';
 		}
 
 		if(c('Linux') && c('ppc64')) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Linux (ppc64)</div>');
+			out+='<div>Syncthing for Linux (ppc64)</div>';
 		}
 
 		//Syncthing for Linux (ppc64le)
 
 		if(c('Mac OS X') && !c(['iPod','iPhone','iPad'])) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Mac OS X (64 bit)</div>');
+			out+='<div>Syncthing for Mac OS X (64 bit)</div>';
 		}
 
 		if(c('NetBSD') && c(bits32)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for NetBSD (32 bit)</div>');
+			out+='<div>Syncthing for NetBSD (32 bit)</div>';
 		}
 
 		if(c('NetBSD') && c(bits64)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for NetBSD (64 bit)</div>');
+			out+='<div>Syncthing for NetBSD (64 bit)</div>';
 		}
 
 		if(c('OpenBSD') && c(bits32)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for OpenBSD (32 bit)</div>');
+			out+='<div>Syncthing for OpenBSD (32 bit)</div>';
 		}
 
 		if(c('OpenBSD') && c(bits64)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for OpenBSD (64 bit)</div>');
+			out+='<div>Syncthing for OpenBSD (64 bit)</div>';
 		}
 
 		if(c('SunOS')) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Solaris/Illumos/SmartOS (64 bit)</div>');
+			out+='<div>Syncthing for Solaris/Illumos/SmartOS (64 bit)</div>';
 		}
 
 		if(c('Win') && !c(bits64)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Windows (32 bit)</div>');
+			out+='<div>Syncthing for Windows (32 bit)</div>';
 		}
 
 		if(c('Win') && c(bits64)) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Syncthing for Windows (64 bit)</div>');
+			out+='<div>Syncthing for Windows (64 bit)</div>';
 		}
 
 		if(c('Android')) {
-			$(outputdiv).append('<div><strong>suggested:</strong> Android App google play or fdroid</div>');
+			out+='<div>Android App google play or fdroid</div>';
 		}
+
+		return out;
 	};
 
 
-	$('#debug-you').html('<div><strong>userAgent:</strong> '+navigator.userAgent+'</div>');
-	ua2dl('#debug-you');
+	$('#debug-you').append('<tr><td>'+navigator.userAgent+'</td><td>'+ua2dl(navigator.userAgent)+'</td></tr>');
 
 
 	// test more agents
 	$('#debug-more').hide(0);
 	for(var i=0; i<test_useragents.length; i++){
-		ua = test_useragents[i];
-		$('#debug-more').append('<div><strong>userAgent:</strong> '+ua+'</div>');
-		ua2dl('#debug-more');
-		$('#debug-more').append('<hr>');
+		$('#debug-more').append('<tr><td>'+test_useragents[i]+'</td><td>'+ua2dl(test_useragents[i])+'</td></tr>');
 	}
 
 	$('#expand-more').click(function(e){
